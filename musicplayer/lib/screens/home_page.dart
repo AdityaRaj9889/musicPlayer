230d8938f6b93aa232815bf9c7e90447d4065d66
CloudCODE8601 <117525115+CloudCODE8601@users.noreply.github.com>
2023-06-14 17:34:33 +0530

    Dev music player (#2)
    * Initial Flutter Counter Template
    
    * music player added
    ui added
    song added
    
    * splash screen added & bug fixed
    
    * carousel added
    
    ---------
    
    Co-authored-by: Aditya <adityaraj9889@gmail.com>

--- musicplayer/lib/screens/home_page.dart
+++ musicplayer/lib/screens/home_page.dart
@@ -1,8 +1,12 @@
 
 
+
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:musicplayer/global.dart';
 import 'package:musicplayer/screens/player_page.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
 
 class MusicHomePage extends StatefulWidget {
   const MusicHomePage({Key? key}) : super(key: key);
@@ -12,6 +16,15 @@ class MusicHomePage extends StatefulWidget {
 }
 
 class _MusicHomePageState extends State<MusicHomePage> {
+
+
+  int activeSlider = 0;
+  List banners = [
+    "assets/banner/b1.jpg",
+    "assets/banner/b2.jpg",
+    "assets/banner/b3.jpg",
+    "assets/banner/b4.jpg"
+  ];
   
   @override
   Widget build(BuildContext context) {
@@ -27,53 +40,113 @@ class _MusicHomePageState extends State<MusicHomePage> {
               ),
         ),
       ),
-      body: ListView.builder(
-        itemCount: GlobalPlayer.songs.length,
-        itemBuilder: (context, i) => Container(
-          margin: const EdgeInsets.symmetric(horizontal: 5),
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            elevation: 5,
-            child: ListTile(
-              onTap: () {
-                setState(() {
-                  GlobalPlayer.index = i;
-                  GlobalPlayer.playSong = GlobalPlayer.songs[i];
-                });
-                Navigator.of(context).push(PageRouteBuilder(
-                    pageBuilder: (_, __, ___) => const PlayerPage()));
-              },
-              leading: Image.asset(
-                "${GlobalPlayer.songs[i]["image"]}",
-                height: 45,
-                width: 45,
-                fit: BoxFit.fill,
+      body: 
+      
+      Column(
+        children: [
+           Stack(
+                alignment: AlignmentDirectional.bottomCenter,
+                children: [
+                  CarouselSlider.builder(
+                      itemCount: banners.length,
+                      itemBuilder: (context, index, realIndex) {
+                        //final sliderImage = sliderList[index];
+                        return Card(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          elevation: 3,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.asset(
+                              banners[index],
+                              fit: BoxFit.cover,
+                              width: double.maxFinite,
+                            ),
+                          ),
+                        );
+                      },
+                      options: CarouselOptions(
+                          height: 150,
+                          autoPlay: true,
+                          // reverse: true,
+                          autoPlayInterval: const Duration(seconds: 2),
+                          viewportFraction: 0.9,
+                          //enlargeCenterPage: true,
+                          enlargeStrategy: CenterPageEnlargeStrategy.height,
+                          onPageChanged: (index, reason) {
+                            setState(() {
+                              activeSlider = index;
+                            });
+                          })),
+                  Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: AnimatedSmoothIndicator(
+                      activeIndex: activeSlider,
+                      count: banners.length,
+                      effect: const ExpandingDotsEffect(
+                        dotHeight: 8,
+                        dotWidth: 8,
+                        activeDotColor: Colors.white,
+                        dotColor: Colors.white54,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              title: SizedBox(
-                height: 20,
-                child: Text(
-                  GlobalPlayer.songs[i]["name"].toString().split(' - ')[0],
-                  style: const TextStyle(
-                      color: GlobalColor.color2,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 17.5),
-                ),
-              ),
-              subtitle: SizedBox(
-                height: 20,
-                child: Text(
-                  GlobalPlayer.songs[i]["name"].toString().split(' - ')[1],
-                  style: const TextStyle(
-                      color: GlobalColor.color3,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400),
+              SizedBox(height: 10,),
+          Expanded(
+            child: ListView.builder(
+              itemCount: GlobalPlayer.songs.length,
+              itemBuilder: (context, i) => Container(
+                margin: const EdgeInsets.symmetric(horizontal: 5),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  elevation: 5,
+                  child: ListTile(
+                    onTap: () {
+                      setState(() {
+                        GlobalPlayer.index = i;
+                        GlobalPlayer.playSong = GlobalPlayer.songs[i];
+                      });
+                      Navigator.of(context).push(PageRouteBuilder(
+                          pageBuilder: (_, __, ___) => const PlayerPage()));
+                    },
+                    leading: Image.asset(
+                      "${GlobalPlayer.songs[i]["image"]}",
+                      height: 45,
+                      width: 45,
+                      fit: BoxFit.fill,
+                    ),
+                    title: SizedBox(
+                      height: 20,
+                      child: Text(
+                        GlobalPlayer.songs[i]["name"].toString().split(' - ')[0],
+                        style: const TextStyle(
+                            color: GlobalColor.color2,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 17.5),
+                      ),
+                    ),
+                    subtitle: SizedBox(
+                      height: 20,
+                      child: Text(
+                        GlobalPlayer.songs[i]["name"].toString().split(' - ')[1],
+                        style: const TextStyle(
+                            color: GlobalColor.color3,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400),
+                      ),
+                    ),
+                  ),
+
                 ),
               ),
             ),
           ),
-        ),
+        ],
+
       ),
     );
   }
